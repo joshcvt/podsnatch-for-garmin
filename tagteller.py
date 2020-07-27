820e421b578c8c3a78d09eeddda52fad1fe2725a
Josh Crockett <josh.crockett@gmail.com>
2020-07-26 20:08:59 -0400

    remove mp3_tagger, use eyed3 instead

--- tagteller.py
+++ tagteller.py
@@ -1,18 +1,21 @@
 #! /usr/bin/env python
 
-from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
+#from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
 from pathlib import Path
+import eyed3
+import traceback
 
-pathlist = Path("dls").rglob('*.mp3')
+# https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
+pathlist = Path("dls/Forgotten_Stories_of_Football").rglob('*.mp3')
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
-    # Create MP3File instance.
-    
-    mp3 = MP3File(path_in_str)
+
     print(path_in_str)
     try:
-        print(mp3.get_tags())
-    except:
-        pass
+        afile = eyed3.load(path_in_str)
+        if afile:
+            print(str(afile.tag))
+    except Exception as ex:
+        traceback.print_exc()
     print("")  # blank ln plz

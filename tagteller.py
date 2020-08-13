4bbc28473697cc63b2647f23dc2e0ed07df026a1
Josh Crockett <josh.crockett@gmail.com>
2020-08-12 23:08:14 -0400

    updated full dump

--- tagteller.py
+++ tagteller.py
@@ -6,16 +6,23 @@ import eyed3
 import traceback
 
 # https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
-pathlist = Path("dls/Forgotten_Stories_of_Football").rglob('*.mp3')
+pathlist = Path("whatsup").rglob('*.mp3')
 for path in pathlist:
     # because path is object not string
     path_in_str = str(path)
 
     print(path_in_str)
     try:
-        afile = eyed3.load(path_in_str)
-        if afile:
-            print(str(afile.tag))
+        audiofile = eyed3.load(path_in_str)
+        if audiofile:
+            print(str(audiofile.tag))
+            print(audiofile.tag.version)
+            print(audiofile.tag.album)
+            print(audiofile.tag.title)
+            print(audiofile.tag.artist)
+            print(audiofile.tag.album_artist)
+            print(audiofile.tag.track_num)
+            print(audiofile.tag.genre)
     except Exception as ex:
         traceback.print_exc()
     print("")  # blank ln plz
